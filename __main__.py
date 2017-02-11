42593dc1fda4f308d314f70ca1a28773b41d3813
Andrew Quintanilla <andyquint@csu.fullerton.edu>
2017-02-11 10:06:09 -0800

    Added demonstration of chord

--- __main__.py
+++ __main__.py
@@ -3,13 +3,21 @@ import numpy
 import pyaudio
 from waves import *
 
-def play_tone(stream, frequency=261.6, length=1, rate=44100):
-	chunks = []
-	output1 = numpy.array(sawtooth(frequency, length, rate))
-	output2 = numpy.array(sawtooth(frequency*5, length, rate))
-	combined = output1 + output2
+def get_wave(frequency=261.6, length=1, rate=44100, type='sine'):
+	output = []
+	if type=='sine':
+		output = numpy.array(sine(frequency, length, rate))
+	elif type=='square':
+		output = numpy.array(square(frequency, length, rate))
+	else:
+		output = numpy.array(sawtooth(frequency, length, rate))
+	
+	return output
 
-	chunks.append(combined)
+def play_tone(output, stream):
+	chunks = []
+
+	chunks.append(output)
 	
 	chunk = numpy.concatenate(chunks) * 0.25
 
@@ -20,7 +28,13 @@ if __name__ == '__main__':
 	stream = p.open(format=pyaudio.paFloat32,
 		channels=1, rate=44100, output=1)
 
-	play_tone(stream)
+	base_freq = 261.6
+
+	output = get_wave(type=sawtooth)
+	output = output + get_wave(frequency=base_freq*math.pow(2,4.0/12),type=sawtooth)
+	output = output + get_wave(frequency=base_freq*math.pow(2,7.0/12),type=sawtooth)
+
+	play_tone(output, stream)
 
 	stream.close()
 	p.terminate()
